e15a1d32aa9a3aa4a48a09d27286665af8a8aec1
Darya <dig14step@gmail.com>
2015-07-06 16:34:46 -0600

    Update Package.cs

--- Package.cs
+++ Package.cs
@@ -1,6 +1,8 @@
-// Author: Darya Ostapenko
-// Date June 29, 2015
-// Usage: Workshop 1. Package class. 
+// OOSD Workshop 2 - Travel Experts
+// July 6, 2015
+//Written by Darya Ostapenko
+//Purpose: creation of Package class with properties and ToString method
+
 
 using System;
 using System.Collections.Generic;
@@ -8,9 +10,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proj2Wkp1
+namespace TravelExpertsData
 {
-    class Package
+
+
+    public class Package
     {
 
         //declare private data
@@ -19,89 +23,79 @@ namespace Proj2Wkp1
         private string pkgDesc;
         private DateTime pkgStartDate;
         private DateTime pkgEndDate;
-        private decimal pkgPrice;
-      
+        private decimal pkgBasePrice;
+        private decimal pkgAgencyCommission;
+
 
         //provide public properties for data
-        
-           public int PackageId
-                {
-                    get {return packageId;}
-                    set {packageId = value;}
-                }
 
-           public string PkgName
-                {
-                    get {return pkgName;}
-                    set {pkgName = value;}
-                }
+        public int PackageId
+        {
+            get { return packageId; }
+            set { packageId = value; }
+        }
 
-           public string PkgDesc
-                {
-                    get {return pkgDesc;}
-                    set {pkgDesc = value;}
-                }
+        public string PkgName
+        {
+            get { return pkgName; }
+            set { pkgName = value; }
+        }
 
-           public DateTime PkgStartDate
-           {
-               get { return pkgStartDate; }
-               set { pkgStartDate = value; }
-           }
+        public string PkgDesc
+        {
+            get { return pkgDesc; }
+            set { pkgDesc = value; }
+        }
 
-           public DateTime PkgEndDate
-           {
-               get { return pkgEndDate; }
-               set { pkgEndDate = value; }
-           }
+        public DateTime PkgStartDate
+        {
+            get { return pkgStartDate; }
+            set { pkgStartDate = value; }
+        }
 
-           public decimal PkgPrice
-           {
-               get { return pkgPrice; }
-               set { pkgPrice = value; }
-           } 
+        public DateTime PkgEndDate
+        {
+            get { return pkgEndDate; }
+            set { pkgEndDate = value; }
+        }
+
+        public decimal PkgBasePrice
+        {
+            get { return pkgBasePrice; }
+            set { pkgBasePrice = value; }
+        }
+
+        public decimal PkgAgencyCommission
+        {
+            get { return pkgAgencyCommission; }
+            set { pkgAgencyCommission = value; }
+        }
 
         //constructors
-           public Package() //default constructor
-           {
-               //declare variables
-               DateTime today;
-               today = DateTime.Now;
-               packageId = 0;
-               pkgName = "";
-               pkgDesc = "";
-               pkgStartDate = today;
-               pkgEndDate = today;
-               pkgPrice = 0;
-           }
+        public Package() //default constructor
+        {
+            //declare variables
+            DateTime today;
+            today = DateTime.Now;
+            packageId = 0;
+            pkgName = "";
+            pkgDesc = "";
+            pkgStartDate = today;
+            pkgEndDate = today;
+            pkgBasePrice = 0;
+            pkgAgencyCommission = 0;
+        }
 
-        public Package(int id, string name, string desc, DateTime start, DateTime end, decimal price) //default constructor
+        public Package(int id, string name, string desc, DateTime start, DateTime end,
+                        decimal price, decimal commission) //default constructor
         {
             packageId = id;
             pkgName = name;
             pkgDesc = desc;
             pkgStartDate = start;
             pkgEndDate = end;
-            pkgPrice = price;
-        }
-
-        public bool addPackage()
-        {
-            return true;
-        }
-
-        public bool editPackage()
-        {
-            return true;
-        }
-
-        public bool deletePackage()
-        {
-            return true;
-        }
-
-        public bool addProductToPackage()
-        {
-            return true;
+            pkgBasePrice = price;
+            pkgAgencyCommission = commission;
         }
 
 
@@ -113,10 +107,11 @@ namespace Proj2Wkp1
                 "Package name:: " + pkgName + "\n" +
                 "Package description: " + pkgDesc + "\n" +
                 "Package start date: " + pkgStartDate.ToString("d MMM yyyy") + "\n" +
-                "Package end date: " + pkgEndDate.ToString("d MMM yyyy") + "\n" + 
-                "Package price: " + pkgPrice.ToString("c"); //display as currency
+                "Package end date: " + pkgEndDate.ToString("d MMM yyyy") + "\n" +
+                "Package price: " + pkgBasePrice.ToString("c") + "]n" + //display as currency
+                "Package agency commission: " + pkgAgencyCommission.ToString("c");
             return s;
         }
-        
+
     }
 }
